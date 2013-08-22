c290215078a77c37e2d5b65222db8cea3f470bfd
driverblock <rick@embedded-designs.com>
2013-08-22 18:34:49 -0400

    Added comments

--- examples/ntpTest/ntpTest.ino
+++ examples/ntpTest/ntpTest.ino
@@ -8,22 +8,18 @@
   please support Adafruit and open-source hardware by purchasing 
   products from Adafruit!
 
-  Written by Kevin Townsend & Limor Fried for Adafruit Industries.  
+  Written by Kevin Townsend & Limor Fried & Rick Lesniak for Adafruit Industries.  
   BSD license, all text above must be included in any redistribution
  ****************************************************/
 
 /*
 
-This example does a full test of core connectivity:
+This example does a test of the SNTP (Simple Network Time Protocol) client:
 * Initialization
 * SSID Scan
 * AP connection
 * DHCP printout
-* DNS lookup
-* Ping
-* Disconnect
-It's a good idea to run this sketch when first setting up the
-module.
+* SNTP time synchronization	
  
 */
 
@@ -50,9 +46,21 @@ Adafruit_CC3000 cc3000 = Adafruit_CC3000(ADAFRUIT_CC3000_CS, ADAFRUIT_CC3000_IRQ
 // Security can be WLAN_SEC_UNSEC, WLAN_SEC_WEP, WLAN_SEC_WPA or WLAN_SEC_WPA2
 #define WLAN_SECURITY   WLAN_SEC_WPA2
 
+//Arguments to SNTP client constructor:
+//	1 - Primary Network Time Server URL (can be NULL)
+//	2 - Secondary Network Time Server URL (also can be NULL)
+//	3 - Local UTC offset in minutes (US Eastern Time is UTC - 5:00
+//	4 - Local UTC offset in minutes for Daylight Savings Time (US Eastern DST is UTC - 4:00
+//	5 - Enable 12-hour mode. true: 12-hour mode, false: 24-hour mode
+//	6 - Enable Daylight Savings Time adjustment (not implemented yet)
+//
 sntp mysntp = sntp(NULL, "time.nist.gov", (short)(-5 * 60), (short)(-4 * 60), true, true);
 
+// Type SNTP_Timestamp is 64-bit NTP time. High-order 32-bits is seconds since 1/1/1900
+//   Low order 32-bits is fractional seconds
 SNTP_Timestamp_t now;
+
+// Type NetTime_t contains NTP time broken out to human-oriented values://	uint16_t millis; ///< Milliseconds after the second (0..999)//	uint8_t	 sec;    ///< Seconds after the minute (0..59)//	uint8_t	 min;    ///< Minutes after the hour (0..59)//	uint8_t	 hour;   ///< Hours since midnight (0..23)//	uint8_t	 mday;   ///< Day of the month (1..31)//	uint8_t	 mon;    ///< Months since January (0..11)//	uint16_t year;   ///< Year.//	uint8_t	 wday;	 ///< Days since Sunday (0..6)//	uint8_t	 yday;   ///< Days since January 1 (0..365)//	bool	 isdst;  ///< Daylight savings time flag, currently not supported	
 NetTime_t timeExtract;
 
 /**************************************************************************/
@@ -116,8 +124,10 @@ void setup(void)
 
   Serial.println(F("UpdateNTPTime"));
   mysntp.UpdateNTPTime();
+
   Serial.println(F("Current local time is:"));
   mysntp.ExtractNTPTime(mysntp.NTPGetTime(&now, true), &timeExtract);
+
   Serial.print(timeExtract.hour); Serial.print(F(":")); Serial.print(timeExtract.min); Serial.print(F(":"));Serial.print(timeExtract.sec); Serial.print(F("."));Serial.println(timeExtract.millis);
   Serial.print(timeExtract.mon); Serial.print(F("-")); Serial.print(timeExtract.mday); Serial.print(F("-"));Serial.println(timeExtract.year);
   Serial.print(F("Day of week: ")); Serial.print(timeExtract.wday); Serial.print(F(", day of year: ")); Serial.println(timeExtract.yday); 
