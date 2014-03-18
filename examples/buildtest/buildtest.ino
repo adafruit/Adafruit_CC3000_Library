3552a6901e0fd6cc945c4beb867b6e77dc3669de
Tony DiCola <tony@tonydicola.com>
2014-03-18 00:22:23 -0700

    Update example code firmware check  to accept latest firmware version.

--- examples/buildtest/buildtest.ino
+++ examples/buildtest/buildtest.ino
@@ -83,7 +83,7 @@ void setup(void)
 */
   
   uint16_t firmware = checkFirmwareVersion();
-  if ((firmware != 0x113) && (firmware != 0x118)) {
+  if ((firmware != 0x113) && (firmware != 0x118) && (firmware != 0x11A)) {
     Serial.println(F("Wrong firmware version!"));
     for(;;);
   }
