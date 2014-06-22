3a5a0cf931490bd5458e549ddec51e3e0ebf55dc
vergun <vergun@gmail.com>
2014-06-22 15:50:47 -0700

    fastrprint: added support for non constant chars for changing messages, useful for POST requests

--- Adafruit_CC3000_Server.h
+++ Adafruit_CC3000_Server.h
@@ -47,6 +47,7 @@ class Adafruit_CC3000_ClientRef : public Print {
 
   size_t fastrprint(const char *str);
   size_t fastrprintln(const char *str);
+  size_t fastrprint(char *str);
   size_t fastrprint(const __FlashStringHelper *ifsh);
   size_t fastrprintln(const __FlashStringHelper *ifsh);
 
