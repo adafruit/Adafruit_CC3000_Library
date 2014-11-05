5ae30d535f4453d955ce50fa1d38f01f4995c9fe
driverblock <rick@embedded-designs.com>
2014-11-05 10:03:11 -0500

    Add test if UpdateNTPTime failed

--- examples/ntpTest/ntpTest.ino
+++ examples/ntpTest/ntpTest.ino
@@ -168,14 +168,15 @@ void setup(void)
   }  
 
   Serial.println(F("UpdateNTPTime"));
-  mysntp.UpdateNTPTime();
+  if (mysntp.UpdateNTPTime())
+  {
+    Serial.println(F("Current local time is:"));
+    mysntp.ExtractNTPTime(mysntp.NTPGetTime(&now, true), &timeExtract);
 
-  Serial.println(F("Current local time is:"));
-  mysntp.ExtractNTPTime(mysntp.NTPGetTime(&now, true), &timeExtract);
-
-  Serial.print(timeExtract.hour); Serial.print(F(":")); Serial.print(timeExtract.min); Serial.print(F(":"));Serial.print(timeExtract.sec); Serial.print(F("."));Serial.println(timeExtract.millis);
-  Serial.print(pF(&dayStrs[timeExtract.wday])); Serial.print(F(", ")); Serial.print(pF(&monthStrs[timeExtract.mon])); Serial.print(F(" ")); Serial.print(timeExtract.mday); Serial.print(F(", "));Serial.println(timeExtract.year);
-  Serial.print(F("Day of year: ")); Serial.println(timeExtract.yday + 1); 
+    Serial.print(timeExtract.hour); Serial.print(F(":")); Serial.print(timeExtract.min); Serial.print(F(":"));Serial.print(timeExtract.sec); Serial.print(F("."));Serial.println(timeExtract.millis);
+    Serial.print(pF(&dayStrs[timeExtract.wday])); Serial.print(F(", ")); Serial.print(pF(&monthStrs[timeExtract.mon])); Serial.print(F(" ")); Serial.print(timeExtract.mday); Serial.print(F(", "));Serial.println(timeExtract.year);
+    Serial.print(F("Day of year: ")); Serial.println(timeExtract.yday + 1); 
+  }
 
   /* You need to make sure to clean up after yourself or the CC3000 can freak out */
   /* the next time you try to connect ... */
